8ea9916e7d5b9cdff843d7e301979f6d75ced164
AtesiT <atesit@mail.ru>
2026-09-13 23:24:26 +0300

    ADD: buttons to new file and open file with shortcuts

--- EditorTXT/EditorTXTApp.swift
+++ EditorTXT/EditorTXTApp.swift
@@ -37,3 +37,29 @@ final class EditorDocument: ObservableObject {
         alert.runModal()
     }
 }
+
+@main
+struct EditorTXTApp: App {
+    @StateObject private var document = EditorDocument()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(document)
+                .frame(minWidth: 500, minHeight: 400)
+        }
+        .commands {
+            CommandGroup(replacing: .newItem) {
+                Button("New") {
+                    document.newDocument()
+                }
+                .keyboardShortcut("n", modifiers: .command)
+
+                Button("Open...") {
+                    document.openFile()
+                }
+                .keyboardShortcut("o", modifiers: .command)
+            }
+        }
+    }
+}
